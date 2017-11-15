674cff153de496759cdddc3f11825f0fc58f0c2c
va1t <peter.maxcy@gmail.com>
2017-11-15 07:57:26 -0500

    only supports fox currently

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 var express = require('express');
-var request = require('request');
 var cheerio = require('cheerio');
+var request = require('request-promise');
 
 var app = express();
 
-var urls = ['http://www.foxnews.com/world.html'];
+var urls = ['http://www.foxnews.com/world'];
 
 app.get('/world', function(req, res) {
+  
+  var results = {};
 
   for(var i=0; i<urls.length; i++) {
-    request(urls[i], function(error, response, html) {
-      if(error) {
-        console.log('['+ urls[i] +'] Error: ' + error );
-      } else {
-        var $ = cheerio.load(html);
-        console.log($);
-      }
-    });
-  }
 
-  res.send('Done.');
+    request(urls[i])
+    .then(function(html) {
+      var $ = cheerio.load(html);
+      $('.title').filter(function(){
+        var entry = $(this).children().first();
+        if(!results.hasOwnProperty(entry.text())) {
+          results[entry.text()] = entry.attr('href');
+        }
+      });      
+    })
+    .then(function(){
+      res.send(results);
+    })
+    .catch(function(err) {
+      console.log('['+ urls[i] +'] Error: ' + error );
+      res.send('ERROR :(');
+    });
+
+  }
 });
 
 app.get('/', function(req, res) {
